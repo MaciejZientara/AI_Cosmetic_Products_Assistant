177a4857914368bf9c47da0d81853accb249f7e8
MaciejZientara <mtzientara@gmail.com>
2024-10-09 20:15:59 +0200

    fix window, comment AI

--- main.py
+++ main.py
@@ -3,10 +3,29 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
-APIKey = ''
-# openai.api_key = APIKey
+####################### graphic ####################### 
 
-client = openai.OpenAI(api_key=APIKey)
+def run_app():
+  app = QApplication(sys.argv)
+  win = QMainWindow()
+  # initial X,Y position on screen, initial X, Y window size 
+  win.setGeometry(200,200,1200,800)
+  win.setWindowTitle("BuisnessAI")
+
+  win.show()
+  sys.exit(app.exec_()) # close python script on app exit
+
+
+####################### functionality - RAG ####################### 
+
+
+
+####################### functionality - openAI ####################### 
+
+# APIKey = ''
+# # openai.api_key = APIKey
+
+# client = openai.OpenAI(api_key=APIKey)
 
 system_prompt = "Wpisz prompt systemowy tutaj"
 context = "Tutaj wiedza asystenta"
@@ -18,33 +37,17 @@ messages=[
   {"role": "assistant", "content": context}
 ]
 
-chat_completion = client.chat.completions.create(
-  messages=messages,
-  model="gpt-4o-mini",
-  temperature=0,
-)
+# chat_completion = client.chat.completions.create(
+#   messages=messages,
+#   model="gpt-4o-mini",
+#   temperature=0,
+# )
 
-print(chat_completion.choices[0].message.content.strip())
+# print(chat_completion.choices[0].message.content.strip())
 # print(chat_completion.usage)
-
-
-
-
-
-def app_init():
-  app = QApplication(sys.argv)
-  win = QMainWindow()
-  # initial X,Y position on screen, initial X, Y window size 
-  win.setGeometry(200,200,300,300)
-  win.setWindowTitle("BuisnessAI")
-
-  sys.exit(app.exec_()) # close python script with app
-  return win
 
 
 ####################### main ####################### 
 
-# initialize
-window = app_init()
-# run app
-window.show()
+if __name__ == '__main__':
+  run_app()
