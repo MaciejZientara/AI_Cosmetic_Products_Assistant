92a972df3535e6fe07a03e325e80f80c44c480c3
MaciejZientara <mtzientara@gmail.com>
2024-11-09 23:16:23 +0100

    added padding to console, working scroll msg

--- src/graphic.py
+++ src/graphic.py
@@ -18,29 +18,47 @@ class customGUI(object):
         self.ClearButton.setObjectName("Clear")
         self.windowGrid.addWidget(self.ClearButton, 0, 2, 1, 1)
         
-        self.textInput = QtWidgets.QLineEdit(Window)
-        self.textInput.setObjectName("textInput")
-        self.windowGrid.addWidget(self.textInput, 2, 0, 1, 3)
-        
         self.ProcessDataButton = QtWidgets.QPushButton(Window)
         self.ProcessDataButton.setObjectName("ProcessData")
         self.windowGrid.addWidget(self.ProcessDataButton, 0, 1, 1, 1)
         
-        self.ProductList = QtWidgets.QScrollArea(Window)
-        self.ProductList.setWidgetResizable(True)
-        self.ProductList.setObjectName("ProductList")
+        self.textInput = QtWidgets.QLineEdit(Window)
+        self.textInput.setObjectName("textInput")
+        self.windowGrid.addWidget(self.textInput, 2, 0, 1, 3)
         
-        self.Console = QtWidgets.QWidget()
+        self.Console = QtWidgets.QScrollArea(Window)
+        self.ConsoleLayoutHolder = QtWidgets.QWidget()
+        self.Console.setWidget(self.ConsoleLayoutHolder)
+
+        self.Console.setWidgetResizable(True)
         self.Console.setGeometry(QtCore.QRect(0, 0, 855, 751))
         self.Console.setObjectName("Console")
+        self.Console.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
+        self.Console.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
         
-        self.ConsoleLayout = QtWidgets.QVBoxLayout(self.Console)
+        self.ConsoleLayout = QtWidgets.QVBoxLayout(self.ConsoleLayoutHolder)
         self.ConsoleLayout.setObjectName("ConsoleLayout")
-        self.ProductList.setWidget(self.Console)
-        self.windowGrid.addWidget(self.ProductList, 1, 0, 1, 3)
+        self.windowGrid.addWidget(self.Console, 1, 0, 1, 3)
+
+        self.initConsole()
 
         self.retranslateUi(Window)
         QtCore.QMetaObject.connectSlotsByName(Window)
+
+
+    def initConsole(self):
+        self.paddingCount = 20
+        for i in range(self.paddingCount):
+            self.addTextLabel("","") # empty labels
+
+        self.addTextLabel("Hello!","AI")
+
+    def clearConsole(self):
+        for i in reversed(range(self.ConsoleLayout.count())): 
+            padding = self.ConsoleLayout.itemAt(i).widget()
+            self.ConsoleLayout.removeWidget(padding)
+            padding.deleteLater()
+            padding = None
 
 
     def addTextLabel(self, text, side):
@@ -57,6 +75,11 @@ class customGUI(object):
         else:
             pass # incorrect side argument
         label.setText(text)
+        #                                    Horizontal                         Vertical
+        size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+        label.setSizePolicy(size_policy)
+        label.setMinimumHeight(25)
+        label.setWordWrap(True)
 
 
     def addItemObject(self, item):
@@ -95,7 +118,15 @@ class customGUI(object):
         self.ProcessDataButton.setText(_translate("Window", "Process Data"))
         
     def processInput(self):
+        if self.paddingCount > 0:
+            self.paddingCount -= 1
+            padding = self.ConsoleLayout.itemAt(0).widget()
+            self.ConsoleLayout.removeWidget(padding)
+            padding.deleteLater()
+            padding = None
+
         self.addTextLabel(self.textInput.text(), "USER")
+        # pass text to AI
         self.textInput.clear()
 
     def connectSignals(self):
